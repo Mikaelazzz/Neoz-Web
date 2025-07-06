c8d608a0b1324ce298bae16da299d854e568ed2d
Mikaela <166426938+Mikaelazzz@users.noreply.github.com>
2025-07-06 23:16:28 +0700

    Redesign homepage layout and add hero cards
    Refactored the homepage to use a new grid layout featuring hero cards with images and types. Added a patch selection dropdown and removed the previous static sections for a more dynamic and updated UI.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,42 +1,40 @@
 import { useState } from 'react'
+import Card from '../components/card_new';
 
 function App() {
 
+  
+  const newHeroes = [
+    { name: "Fanny", type1: "Chase", type2: "Reap", image: "/src/media/TESss.png" },
+    { name: "Layla", type1: "Reap", type2: "Damage", image: "/src/media/TESss.png" },
+    { name: "Fanny", type1: "Chase", type2: "Reap", image: "/src/media/TESss.png" },
+    { name: "Layla", type1: "Reap", type2: "Damage", image: "/src/media/TESss.png" },
+    { name: "Fanny", type1: "Chase", type2: "Reap", image: "/src/media/TESss.png" },
+    { name: "Layla", type1: "Reap", type2: "Damage", image: "/src/media/TESss.png" }
+  ];
+
   return (
     <>
-    <section className='p-4 font-[poppins] space-y-6'>
-        <div className='flex justify-center text-center font-[poppins] bg-sky-200 p-4 rounded-lg'>
-          <div className='space-y-2'>
-            <img src="src/media/logo.png" alt="" className='w-32 h-32'/>
-            <h1 >Welcome To Neoz</h1>
-          </div>
+    <section className='p-4 font-[poppins] space-y-6  mx-12 mt-4'>
+        <div className='flex flex-row justify-between items-center rounded-md'>
+          <h1 className='text-sm font-bold mb-4 xl:text-xl items-center'>New Update Patch On MLBB !</h1>
+          <select className='bg-gray-300 p-1 rounded-lg' name="" id="">
+            <option value="" hidden>Pilih Patch</option>
+            <option value="1">1.9.4.2</option>
+            <option value="2">1.9.4.1</option>
+            <option value="3">1.9.4.0</option>
+          </select>
         </div>
-        <div className='flex justify-center font-[poppins] bg-sky-200 p-4 rounded-lg'>
-          <div className='flex flex-row gap-2'>
-            <img src="src/media/logo.png" alt="" className='w-32 h-32'/>
-            <div className='my-auto w-52'>
-            <h1 className='font-bold'>PRAKTIS UI</h1>
-            <small>Tampilan yang mudah dipahami</small>
-            </div>
-          </div>
-        </div>
-        <div className='flex justify-center font-[poppins] bg-sky-200 p-4 rounded-lg'>
-          <div className='flex flex-row gap-2'>
-            <img src="src/media/logo.png" alt="" className='w-32 h-32'/>
-            <div className='my-auto w-52'>
-            <h1 className='font-bold'>CEK WIN RATE</h1>
-            <small>Untuk Mengetahui Win Rate Hero</small>
-            </div>
-          </div>
-        </div>
-        <div className='flex justify-center font-[poppins] bg-sky-200 p-4 rounded-lg'>
-          <div className='flex flex-row gap-2'>
-            <img src="src/media/logo.png" alt="" className='w-32 h-32'/>
-            <div className='my-auto w-52'>
-            <h1 className='font-bold'>INFORMASI PATCH</h1>
-            <small>Memberikan Informasi tentang Patch Terbaru</small>
-            </div>
-          </div>
+        <div className='grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-3'>
+          {newHeroes.map((hero,index)=>(
+            <Card 
+              key={index} 
+              name={hero.name} 
+              type1={hero.type1} 
+              type2={hero.type2} 
+              image={hero.image} 
+            />
+          ))}
         </div>
     </section>
     
